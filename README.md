8ae2eba9927b8ef2cce4a522e80330770d97bdfa
Derek Land <derek@ddmr.nl>
2016-12-18 22:37:37 +0100

    Module om figuur-assen goed weer te geven
    Module past aan de hand van 2 variabelen de weergave van significante getallen op de assen van een matplotlib figuur aan. Punten als decimaal teken worden vervangen door kommas en 10 machten als superscript weergegeven (met hulp van LaTeX code).

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # TIS-TN-python
+
+Hier staan python scripts en modules die kunnen helpen gedurende de studie TN bij de Haagse Hogeschool.
+
+## TIS-TN-plot.py
+
+Script om significante cijfers en gebruik van machten volgens het document Labjournaal, Meetrapport en Verslag weer te geven. \\
+Punten zijn vervangen door komma's en X- en Y-as zijn afzonderlijk van precisie te voorzien.
+
+### Gebruik
+import matplotlib.pyplot as plt
+import TIS-TN-plot as hhs
+hhs.PRECISION_Y = 3 # zet y-as op 3 cijfes significant (standaard = 2)
+
+fix_axis(plt.gca()) # zet de assen juist neer
+plt.tight_layout() # zet de figuur margins goed
---
+++ TIS-TN-plot.py
@@ -0,0 +1,85 @@
+def to_precision(x,p):
+    """
+    returns a string representation of x formatted with a precision of p
+    Based on the webkit javascript implementation taken from here:
+    https://code.google.com/p/webkit-mirror/source/browse/JavaScriptCore/kjs/number_object.cpp
+    """
+    import math
+    x = float(x)
+
+    if x == 0.:
+        if (p-1) > 0:
+            return '$0,\!' + '0'*(p-1) +'$'
+        else:
+            return '$0$'
+    out = []
+
+    if x < 0:
+        out.append("-")
+        x = -x
+
+    e = int(math.log10(x))
+    tens = math.pow(10, e - p + 1)
+    n = math.floor(x/tens)
+
+    if n < math.pow(10, p - 1):
+        e = e -1
+        tens = math.pow(10, e - p+1)
+        n = math.floor(x / tens)
+
+    if abs((n + 1.) * tens - x) <= abs(n * tens -x):
+        n = n + 1
+
+    if n >= math.pow(10,p):
+        n = n / 10.
+        e = e + 1
+        
+    m = "%.*g" % (p, n)
+
+    if e < -2 or e >= p:
+        out.append(m[0])
+        if p > 1:
+            out.append(",\!")
+            out.extend(m[1:p])
+        out.append('\cdot 10^{')
+        out.append(str(e))
+        out.append('}')
+    elif e == (p -1):
+        out.append(m)
+    elif e >= 0:
+        out.append(m[:e+1])
+        if e+1 < len(m):
+            out.append(",\!")
+            out.extend(m[e+1:])
+    else:
+        out.append("0,")
+        out.extend(["0"]*-(e+1))
+        out.append(m)
+    outstr = "".join(out)
+    return '$' + outstr + '$'
+
+PRECISION_Y = 2 # toegang tot variabele buiten module
+def funcy(data, pos):
+    return to_precision(data,PRECISION_Y)
+
+PRECISION_X = 2 # toegang tot variabele buiten module
+def funcx(data, pos):
+    return to_precision(data,PRECISION_X)
+
+from matplotlib.ticker import FuncFormatter
+def fix_axis(ax):
+	formattery = FuncFormatter(funcy)
+	formatterx = FuncFormatter(funcx)
+	ax.yaxis.set_major_formatter(FuncFormatter(formattery))
+	ax.xaxis.set_major_formatter(FuncFormatter(FuncFormatter(formatterx)))
+
+def test_to_precision():
+    numbers = [0, 0.1, 0.4, 0.232, 0.0, 1.242323, 9.999, -1343213.234]
+    for number in numbers:
+        print(number, 3, to_precision(number, 3))
+        print(number, 7, to_precision(number, 7))
+        print(number, 1, to_precision(number, 1))
+		
+if __name__ == '__main__':
+    test_to_precision()
+    
