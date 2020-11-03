6a2f018bc55a4f7343f94fda3b153726e733c9e6
Derek Land <derek@ddmr.nl>
2020-11-03 20:12:24 +0100

    update in setup

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+import pathlib
 import setuptools 
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name='TN_code',
-    version='0.91',
+    version='0.92',
     description='Code gebruikt bij de opleiding TN van de Haagse Hogeschool',
+    long_description=README,
+    long_description_content_type="text/markdown",      
     url='https://hhs-tn.github.io',
     license='MIT',
     author='Derek Land',
     author_email='d.d.land@hhs.nl',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('voorbeelden'),),
     install_requires=['numpy', 
                       'matplotlib',
                       'pandas',
