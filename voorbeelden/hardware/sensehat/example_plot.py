ce57dfe72b9b4f22c9c321d441159085c1cad230
Derek Land <derek@ddmr.nl>
2021-01-18 12:50:42 +0100

    update in voorbeeld plot code

--- voorbeelden/hardware/sensehat/example_plot.py
+++ voorbeelden/hardware/sensehat/example_plot.py
@@ -4,10 +4,13 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from TN_code.hardware import lees_data_numpy
 
-    # data van 1 kanaal weergeven
+    # geef de filename op!
     fn = 'filename.txt'
+
     data = lees_data_numpy.readArray(fn)
     time = data[:, 0]
     print('Samplerate: ', len(time)/(time[-1]-time[0]))
     plt.plot(time, data[:, 1])
+    plt.plot(time, data[:, 2])
+    plt.plot(time, data[:, 3])
     plt.show()
