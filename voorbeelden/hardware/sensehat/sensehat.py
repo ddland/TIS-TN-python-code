13336cd300ba23eb1c9f886920fe8dc921fdd18c
Arjow <arjow@protonmail.com>
2022-09-02 19:49:38 +0200

    open file 'with with', removed Exception Handling
    open file with 'with open(file)' and removed  try except handling

--- voorbeelden/hardware/sensehat/sensehat.py
+++ voorbeelden/hardware/sensehat/sensehat.py
@@ -18,12 +18,10 @@ if __name__ == '__main__':
     # geef de file een naam met de huidige tijd.
     filename = 'ACC_%s.txt' % (time.strftime('%Y%m%d-%H%M%S',
                                time.localtime()))
-    # open de file in 'append' mode.
-    stream = open(filename, 'a')
+
 
     # maak het sense object aan en lees uit de RTIMULib file het
     # ingestelde span.
-
     sense = SenseHat()
     rtimulib_config = sense._get_settings_file('RTIMULib')
     accel_span = rtimulib_config.LSM9DS1AccelFsr
@@ -32,9 +30,9 @@ if __name__ == '__main__':
     sense.show_message(filename)
     sense.show_letter('%s' % (accel_span))
 
-    # start de hoofd-lus.
-    run = True
-    try:  # bij CTRL-C breek dan de code af.
+
+    # open de file in 'append' mode.
+    with open(filename, 'a') as stream:
         while run:
             # lees de ACC data en schrijf die weg naar de stream.
             data = sense.get_accelerometer_raw()
@@ -42,12 +40,3 @@ if __name__ == '__main__':
                                             data['x'],
                                             data['y'],
                                             data['z']))
-    except KeyboardInterrupt:
-        # breek de while-loop af
-        run = False
-
-    # sluit het programma netjes af (na einde lus).
-    stream.close()
-    sense.clear()
-    time.sleep(1)
-    sys.exit()
